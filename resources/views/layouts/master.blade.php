b1356c772e0ef1e4c0e03cbdfa4a358807a7e841
Ross Wintle <ross@oikos.org.uk>
2018-05-28 22:27:19 +0100

    Remove analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -82,21 +82,13 @@
 
     <footer>
         <div class="container centered">
-            <p>An <a href="https://oikos.digital">Oikos Digital</a> Project</p>
-            <p>I'm using cookies for Analytics. You need to know.</p>
+            <p>An <a href="https://oikos.digital/">Oikos Digital</a> Project</p>
+            <p>This site uses <a href="https://blog.kownter.com/about-kownter/">Kownter</a> for analytics.
+                This does not use cookies and does not collect any personal data.
+                <a href="https://blog.kownter.com/about-kownter/">Find out more</a>.</p>
             <p>Contains public sector information licensed under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0.</a></p>
         </div>
     </footer>
-
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-12614570-20', 'auto');
-        ga('send', 'pageview');
-    </script>
 
     @if ( env('KOWNTER_APP_URL') )
         <script>
